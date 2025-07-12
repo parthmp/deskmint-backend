6cc30d83f3ce10ce4021b5fec8e95e4b3e4c2790
parthmp <parthparmaronline@gmail.com>
2025-07-13 00:13:22 +0530

    cors and csrf

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    
+	public function login(Request $request){
+
+		echo Hash::make('pass123');
+		die();
+
+		$user = User::where('id', '=', 1)->first();
+		
+		/*
+		if(Auth::check($user)){
+			
+		}*/
+
+		$token = $user->createToken('admin_token');
+		dd($token);
+
+	}
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/test', function (Request $request) {
-    return 'api test';
+
+Route::post('/some/thing', function(\Illuminate\Http\Request $request){
+	return response()->json([
+		'bla' => 123
+	]);
+});
+
+Route::post('/debug-csrf', function (\Illuminate\Http\Request $request) {
+    return response()->json([
+        'session_started' => session()->has('_token'),
+        'session_csrf_token' => session()->token(),
+        'header_csrf_token' => $request->header('X-XSRF-TOKEN'),
+        'cookie_csrf_token' => $request->cookie('XSRF-TOKEN'),
+        'all_cookies' => request()->cookies->all(),
+        'all_headers' => request()->headers->all(),
+        'session_id' => session()->getId(),
+    ]);
 });
