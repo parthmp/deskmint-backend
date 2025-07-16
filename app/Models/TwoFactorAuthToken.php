9020899240faacbe26a803fe55d382a9bc916de7
parthmp <parthparmaronline@gmail.com>
2025-07-17 02:44:09 +0530

    day end

--- app/Models/TwoFactorAuthToken.php
+++ app/Models/TwoFactorAuthToken.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TwoFactorAuthToken extends Model
-{
+{	
+	use SoftDeletes;
+
     protected $table = '2fa_tokens';
+
+	public function user(){
+		return $this->belongsTo(User::class, 'user_id');
+	}
+
 }
