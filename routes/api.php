b5e4bcebd8f191b982b6d2d4bbc0cfe82531b231
parthmp <parthparmaronline@gmail.com>
2025-07-19 02:59:44 +0530

    cleanup

--- routes/api.php
+++ routes/api.php
@@ -1,33 +1,9 @@
 <?php
 
 use App\Http\Controllers\LoginController;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');*/
 
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/resend-otp', [LoginController::class, 'resendOTP']);
 Route::post('/validate-otp', [LoginController::class, 'validateOTP']);
-Route::get('/temp', 
-	function(){
-		return view('emails.send_otp');
-	}
-);
-
-/*
-Route::post('/debug-csrf', function (\Illuminate\Http\Request $request) {
-    return response()->json([
-        'session_started' => session()->has('_token'),
-        'session_csrf_token' => session()->token(),
-        'header_csrf_token' => $request->header('X-XSRF-TOKEN'),
-        'cookie_csrf_token' => $request->cookie('XSRF-TOKEN'),
-        'all_cookies' => request()->cookies->all(),
-        'all_headers' => request()->headers->all(),
-        'session_id' => session()->getId(),
-    ]);
-});*/
