5c33dcaf6138518091ae751e6de952a530b59933
parthmp <parthparmaronline@gmail.com>
2025-08-02 16:24:40 +0530

    config changes

--- config/global.php
+++ config/global.php
@@ -2,7 +2,7 @@
 
 	return [
 		'otp_expiry' 			=> 	env('OTP_EXPIRY_IN_SECONDS', 600),
-		'reset_code_expiry' 	=> 	env('RESET_CODE_EXPIRY_IN_SECONDS', 60),
+		'reset_code_expiry' 	=> 	env('RESET_CODE_EXPIRY_IN_SECONDS', 600),
 		'error_code'			=>	env("ERROR_CODE", 422),
 		'user_types'			=>	[
 										'admin'		=>	1,
