311c06d7d68a4bfebdddb60af85fec897399c607
parthmp <parthparmaronline@gmail.com>
2025-08-19 02:36:16 +0530

    currencies

---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Currency;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void{
+        $currencies = config('currencies');
+
+		foreach($currencies as $currency){
+			Currency::create($currency);
+		}
+    }
+}
