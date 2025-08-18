1388abf26b7524b7e2e9d8dfae815d57d7682e74
parthmp <parthparmaronline@gmail.com>
2025-08-19 00:19:01 +0530

    rows printing

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -12,10 +12,72 @@ class ClientsController extends Controller{
 
 		$company_id = Sanitize::input($request->input('company_id'));
 
-		$fields = ClientsCustomField::where('company_id', '=', $company_id)->whereHas('customFieldType')->orderBy('order_on_add_edit_page', 'asc')->with('customFieldType')->get();
+		$fields = ClientsCustomField::where('company_id', '=', $company_id)->whereHas('customFieldType')->orderBy('id', 'asc')->with('customFieldType')->get();
 
-		return $fields;
+		return $this->adjustRowsPrinting($fields);
 
 	}
 
+	private function adjustRowsPrinting($fields){ 
+
+		$full_width_types = [
+			config('global.field_types')[1],
+			config('global.field_types')[9]
+		];
+
+		$rows = [];
+		$current_row = [];
+
+		foreach($fields as $field){
+
+			$current_type = $field->customFieldType->input_type;
+
+			if(in_array($current_type, $full_width_types)){
+				
+				if(!empty($current_row)){
+					$rows[] = $current_row;
+					$current_row = [];
+				}
+				
+				$rows[] = [$field];
+
+			}else{
+
+				$current_row[] = $field;
+				if(count($current_row) == 3){
+					$rows[] = $current_row;
+					$current_row = [];
+				}
+			
+			}
+
+		}
+
+		if(!empty($current_row)){
+			$rows[] = $current_row;
+		}
+
+		foreach($rows as $row){
+
+			$count = count($row);
+			$span = 12;
+			
+			if($count === 2){
+				$span = 6;
+			}
+			
+			if($count === 3){
+				$span = 4;
+			}
+			
+			foreach ($row as $field) {
+				$field->span = $span; 
+			}
+
+		}
+		
+		return collect($rows)->flatten();
+
+	}
+	
 }
