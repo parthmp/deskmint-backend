0a55c41bf230c3f46bd4f400cca106e36419c825
parthmp <parthparmaronline@gmail.com>
2025-09-18 06:21:22 +0530

    modified ArrangedColumns trait

--- app/Traits/ArrangedColumns.php
+++ app/Traits/ArrangedColumns.php
@@ -7,7 +7,9 @@ use App\Helpers\Sanitize;
 use App\Models\SettingsIndexColumn;
 use App\Models\UserIndexColumn;
 use Exception;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
@@ -89,127 +91,171 @@ trait ArrangedColumns{
 
 	}
 
-	protected function fetchArrangedColumnsData(Request $request, string $feature_name, string $original_table, string $custom_fields_model, string $type) : array{
-		
-		$company_id = Sanitize::input($request->input('company_id'));
+	private function getSavedColumnData(int $company_id, string $feature_name) : mixed{
 
-		/* check if user has any data */
 		$user_data = UserIndexColumn::where([['user_id', '=', Auth::user()->id], ['company_id', '=', $company_id], ['feature_name', '=', $feature_name]])->first();
 
 		if(!$user_data){
 			$user_data = SettingsIndexColumn::where([['company_id', '=', $company_id], ['feature_name', '=', $feature_name]])->first();
 		}
 
+		return $user_data;
+
+	}
+
+	private function parseSavedData(string $columns_json, string $type, string $custom_fields_model, int $company_id, array $table_columns) : array{
+
+		$user_fields = [];
+
+		$fields_json = json_decode($columns_json, true);
+
+		$saved_labels_normal = [];
+		$saved_ids_custom = [];
+		
+		$splitted = $this->splitFields($fields_json, $type);
+		$saved_labels_normal = $splitted['normal'];
+		$saved_ids_custom = $splitted['custom'];
+
+		$general_custom_columns_ids = $custom_fields_model::where('company_id', '=', $company_id)->pluck('id')->toArray();
+		
+		foreach($fields_json as $field){
+
+			if(in_array($field['label'], $table_columns) || in_array($field[$type.'s_custom_fields_id'], $general_custom_columns_ids)){
+				$user_fields[] = $field;
+			}
+
+		}
+
+		$counter = 1;
+		foreach($table_columns as $temp_general_column){
+			$to_push = [];
+			if(!in_array($temp_general_column, $saved_labels_normal)){
+				$to_push['id'] = $counter++;
+				$to_push['label'] = $temp_general_column;
+				$to_push['is_date'] = false;
+				$to_push['text'] = General::NormalizeColumnName($temp_general_column);
+				if($temp_general_column === 'created_at'){
+					$to_push['text'] = 'Added on';
+					$to_push['is_date'] = true;
+				}
+				$to_push['type'] = 'normal';
+				$to_push['searchable'] = false;
+				$to_push['show'] = false;
+				
+				$user_fields[] = $to_push;
+			}
+		}
+
+		return [
+			'user_fields'		=>		$user_fields,
+			'saved_ids_custom'	=>		$saved_ids_custom,
+			'counter'			=>		$counter
+		];
+
+	}
+
+	private function modifyParsedSavedData(array $user_fields, array $general_custom_columns, string $type) : array{
+
+		for($z = 0 ; $z < count($user_fields) ; $z++){
+
+			if($user_fields[$z]['type'] === 'custom'){
+
+				for($x = 0 ; $x < count($general_custom_columns) ; $x++){
+
+					if($general_custom_columns[$x]['id'] === $user_fields[$z][$type.'s_custom_fields_id']){
+
+						$user_fields[$z]['text'] = General::NormalizeColumnName($general_custom_columns[$x]['label']);
+						$user_fields[$z]['is_date'] = false;
+						if($general_custom_columns[$x]['custom_field_type']['input_type'] === config('global.field_types')[5] || $general_custom_columns[$x]['custom_field_type']['input_type'] === config('global.field_types')[7]){
+							$user_fields[$z]['is_date'] = true;
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+
+		return $user_fields;
+
+
+	}
+
+	private function addNotSavedAndLaterAddedColumns(array $general_custom_columns, array $saved_ids_custom, int $counter, string $type) : array{
+
+		$user_fields = [];
+
+		foreach($general_custom_columns as $t_gen_custom_columns){
+			
+			$to_push = [];
+
+			if($t_gen_custom_columns['custom_field_type']['input_type'] !== config('global.field_types')[9]){
+
+				if(!in_array($t_gen_custom_columns['id'], $saved_ids_custom)){
+
+					$to_push['id'] = $counter++;
+					$to_push['label'] = '-';
+					$to_push['text'] = General::NormalizeColumnName($t_gen_custom_columns['label']);
+					$to_push['type'] = 'custom';
+					$to_push[$type.'s_custom_fields_id'] = $t_gen_custom_columns['id'];
+					$to_push['searchable'] = false;
+					$to_push['show'] = false;
+					$to_push['is_date'] = false;
+					if($t_gen_custom_columns['custom_field_type']['input_type'] === config('global.field_types')[5] || $t_gen_custom_columns['custom_field_type']['input_type'] === config('global.field_types')[7]){
+						$to_push['is_date'] = true;
+					}
+
+					$user_fields[] = $to_push;
+
+				}
+			}
+		}
+
+		return $user_fields;
+
+	}
+
+	protected function fetchArrangedColumnsData(Request $request, string $feature_name, string $original_table, string $custom_fields_model, string $type) : array{
+		
+		$company_id = Sanitize::input($request->input('company_id'));
+
+		/* check if user has any data */
+		$user_data = $this->getSavedColumnData($company_id, $feature_name);
+
 		/* fetch all fields */
 		$table_columns = Schema::getColumnListing($original_table);
 		$table_columns = array_values(array_diff($table_columns, ['deleted_at', 'updated_at']));
 		
-		$clients_custom_columns = $custom_fields_model::where('company_id', '=', $company_id)->whereHas('customFieldType')->with('customFieldType')->get()->toArray();
+		$general_custom_columns = $custom_fields_model::where('company_id', '=', $company_id)->whereHas('customFieldType')->with('customFieldType')->get()->toArray();
 
 		$user_fields = [];
 
 		/* handle userdata here */
 		if($user_data){
-			
-			$fields_json = json_decode($user_data->columns_json, true);
 
-			$saved_labels_normal = [];
-			$saved_ids_custom = [];
-			
-			$splitted = $this->splitFields($fields_json, $type);
-			$saved_labels_normal = $splitted['normal'];
-			$saved_ids_custom = $splitted['custom'];
+			$temp = $this->parseSavedData($user_data->columns_json, $type, $custom_fields_model, $company_id, $table_columns);
+			$user_fields = $temp['user_fields'];
+			$saved_ids_custom = $temp['saved_ids_custom'];
+			$counter = $temp['counter'];
 
-			$table_columns = Schema::getColumnListing($original_table);
-			$table_columns = array_values(array_diff($table_columns, ['deleted_at', 'updated_at']));
-			
-			$clients_custom_columns_ids = $custom_fields_model::where('company_id', '=', $company_id)->pluck('id')->toArray();
-			
-			foreach($fields_json as $field){
+			$user_fields = $this->modifyParsedSavedData($user_fields, $general_custom_columns, $type);
 
-				if(in_array($field['label'], $table_columns) || in_array($field[$type.'s_custom_fields_id'], $clients_custom_columns_ids)){
-					$user_fields[] = $field;
-				}
-
-			}
-
-			$counter = 1;
-			foreach($table_columns as $temp_client_column){
-				$to_push = [];
-				if(!in_array($temp_client_column, $saved_labels_normal)){
-					$to_push['id'] = $counter++;
-					$to_push['label'] = $temp_client_column;
-					$to_push['is_date'] = false;
-					$to_push['text'] = General::NormalizeColumnName($temp_client_column);
-					if($temp_client_column === 'created_at'){
-						$to_push['text'] = 'Added on';
-						$to_push['is_date'] = true;
-					}
-					$to_push['type'] = 'normal';
-					$to_push['searchable'] = false;
-					$to_push['show'] = false;
-					
-					$user_fields[] = $to_push;
-				}
-			}
-
-			/* modify text to show */
-			for($z = 0 ; $z < count($user_fields) ; $z++){
-
-				if($user_fields[$z]['type'] === 'custom'){
-
-					for($x = 0 ; $x < count($clients_custom_columns) ; $x++){
-
-						if($clients_custom_columns[$x]['id'] === $user_fields[$z][$type.'s_custom_fields_id']){
-
-							$user_fields[$z]['text'] = General::NormalizeColumnName($clients_custom_columns[$x]['label']);
-							$user_fields[$z]['is_date'] = false;
-							if($clients_custom_columns[$x]['custom_field_type']['input_type'] === config('global.field_types')[5] || $clients_custom_columns[$x]['custom_field_type']['input_type'] === config('global.field_types')[7]){
-								$user_fields[$z]['is_date'] = true;
-							}
-
-						}
-
-					}
-
-				}
-
-			}
-
-			/*  */
-
-			foreach($clients_custom_columns as $t_clients_custom_columns){
-				$to_push = [];
-				if($t_clients_custom_columns['custom_field_type']['input_type'] !== config('global.field_types')[9]){
-					if(!in_array($t_clients_custom_columns['id'], $saved_ids_custom)){
-						$to_push['id'] = $counter++;
-						$to_push['label'] = '-';
-						$to_push['text'] = General::NormalizeColumnName($t_clients_custom_columns['label']);
-						$to_push['type'] = 'custom';
-						$to_push[$type.'s_custom_fields_id'] = $t_clients_custom_columns['id'];
-						$to_push['searchable'] = false;
-						$to_push['show'] = false;
-						$to_push['is_date'] = false;
-						if($t_clients_custom_columns['custom_field_type']['input_type'] === config('global.field_types')[5] || $t_clients_custom_columns['custom_field_type']['input_type'] === config('global.field_types')[7]){
-							$to_push['is_date'] = true;
-						}
-						$user_fields[] = $to_push;
-					}
-				}
-			}
+			$user_fields = array_merge($user_fields, $this->addNotSavedAndLaterAddedColumns($general_custom_columns, $saved_ids_custom, $counter, $type));
 
 			return $user_fields;
 			
 		}else{
-			return $this->nonUserDataColumns($table_columns, $clients_custom_columns, $type);
+			return $this->nonUserDataColumns($table_columns, $general_custom_columns, $type);
 		}
 		
 
 
 	}
 
-	protected function saveArrangedColumnsData(Request $request, string $custom_fields_model, string $feature_name, string $original_table, string $type){
-		
+	private function validatePostedColumns(Request $request){
+
 		$validation_rules = [
 			'columns'	 =>	'required|array'
 		];
@@ -220,6 +266,17 @@ trait ArrangedColumns{
 			return response(['message' => 'Invalid request', 'validity' => 'invalid_request'], config('global.error_code'));
 		}
 
+		return null;
+
+	}
+
+	protected function saveArrangedColumnsData(Request $request, string $custom_fields_model, string $feature_name, string $original_table, string $type) : Response{
+		
+		$validated = $this->validatePostedColumns($request);
+		if($validated !== null){
+			return $validated;
+		}
+
 		$columns = $request->input('columns');
 		$company_id = Sanitize::input($request->input('company_id'));
 
@@ -228,16 +285,16 @@ trait ArrangedColumns{
 			return response(['message' => 'Saved successfully', 'validity' => 'saved_success'], 200);
 		}
 
-		$clients_columns = Schema::getColumnListing($original_table);
-		$clients_columns = array_values(array_diff($clients_columns, ['deleted_at', 'updated_at']));
+		$general_columns = Schema::getColumnListing($original_table);
+		$general_columns = array_values(array_diff($general_columns, ['deleted_at', 'updated_at']));
 		
-		$clients_custom_column_ids = $custom_fields_model::where('company_id', '=', $company_id)->pluck('id')->toArray();
+		$general_custom_column_ids = $custom_fields_model::where('company_id', '=', $company_id)->pluck('id')->toArray();
 
 		for($z = 0 ; $z < count($columns) ; $z++){
 			if(!isset($columns[$z][$type.'s_custom_fields_id'])){
 				$columns[$z][$type.'s_custom_fields_id'] = '-';
 			}
-			if(!in_array($columns[$z]['label'], $clients_columns) && !in_array($columns[$z][$type.'s_custom_fields_id'], $clients_custom_column_ids)){
+			if(!in_array($columns[$z]['label'], $general_columns) && !in_array($columns[$z][$type.'s_custom_fields_id'], $general_custom_column_ids)){
 				return response(['message' => 'Invalid request', 'validity' => 'invalid_request'], config('global.error_code'));
 			}
 
