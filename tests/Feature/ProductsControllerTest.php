c70056e44853d1ece83dea90dc98590bf02d05a9
parthmp <parthparmaronline@gmail.com>
2025-09-22 17:18:06 +0530

    product update tests

--- tests/Feature/ProductsControllerTest.php
+++ tests/Feature/ProductsControllerTest.php
@@ -23,6 +23,22 @@ class ProductsControllerTest extends TestCase{
 
 	}
 
+	private function addNewProduct(int $company_id) : Product{
+		
+		Product::truncate();
+
+		$product = new Product();
+		$product->company_id = $company_id;
+		$product->product_name = 'test product here';
+		$product->price = 9.98;
+		$product->sku = 'some sku';
+		$product->description = 'description here';
+		$product->save();
+
+		return $product;
+
+	}
+
 	public function test_if_it_fails_to_save_product_with_invalid_data_1() : void{
 
 		$c = $this->set_access('device 123');
@@ -170,7 +186,170 @@ class ProductsControllerTest extends TestCase{
 		$this->assertNull($product->deleted_at);
 
 	}
+
+	public function test_if_it_fails_to_update_product_with_invalid_product_id() : void{
+
+		$c = $this->set_access('device 123');
+		$company_id = $this->set_default_company();
+
+		$response = $this->patch('/api/manage-products/100', [
+			'product_name'				=>	'',
+			'company_id'				=>	$company_id
+		], $c['headers']);
+
+		$response->assertStatus((int)config('global.error_code'));
+
+		$json = $response->json();
+
+		$this->arrayHasKey('validity', $json);
+		$this->assertEquals('invalid_request', $json['validity']);
+
+	}
 	
+
+	public function test_if_it_fails_to_update_product_with_invalid_data_1() : void{
+
+		$c = $this->set_access('device 123');
+		$company_id = $this->set_default_company();
+
+		$product = $this->addNewProduct($company_id);
+
+		$response = $this->patch('/api/manage-products/'.$product->id, [
+			'product_name'				=>	'',
+			'company_id'				=>	$company_id
+		], $c['headers']);
+
+		$response->assertStatus((int)config('global.error_code'));
+
+		$json = $response->json();
+
+		$this->arrayHasKey('validity', $json);
+		$this->assertEquals('invalid_data', $json['validity']);
+
+	}
+
+	public function test_if_it_fails_to_update_product_with_invalid_data_2() : void{
+
+		$c = $this->set_access('device 123');
+		$company_id = $this->set_default_company();
+
+		$product = $this->addNewProduct($company_id);
+
+		$response = $this->patch('/api/manage-products/'.$product->id, [
+			'product_name'				=>	'       ',
+			'company_id'				=>	$company_id
+		], $c['headers']);
+
+		$response->assertStatus((int)config('global.error_code'));
+
+		$json = $response->json();
+
+		$this->arrayHasKey('validity', $json);
+		$this->assertEquals('invalid_data', $json['validity']);
+
+	}
+
+	public function test_if_it_fails_to_update_product_with_invalid_data_3() : void{
+
+		$c = $this->set_access('device 123');
+		$company_id = $this->set_default_company();
+
+		$product = $this->addNewProduct($company_id);
+
+		$response = $this->patch('/api/manage-products/'.$product->id, [
+			'company_id'				=>	$company_id
+		], $c['headers']);
+
+		$response->assertStatus((int)config('global.error_code'));
+
+		$json = $response->json();
+
+		$this->arrayHasKey('validity', $json);
+		$this->assertEquals('invalid_data', $json['validity']);
+
+	}
 	
+	public function test_if_it_updates_product_with_valid_data_with_required_data_only() : void{
+
+		$c = $this->set_access('device 123');
+		$company_id = $this->set_default_company();
+
+		$product = $this->addNewProduct($company_id);
+		
+		$response = $this->patch('/api/manage-products/'.$product->id, [
+			'product_name'				=>	'updated product name here',
+			'company_id'				=>	$company_id
+		], $c['headers']);
+
+		$response->assertStatus(200);
+
+		$json = $response->json();
+
+		$this->arrayHasKey('validity', $json);
+		$this->assertEquals('updated_success', $json['validity']);
+
+		$updated_product = Product::where('id', '=', $product->id)->first();
+		$this->assertEquals('updated product name here', $updated_product->product_name);
+		
+	}
+
+	public function test_if_it_updates_product_with_valid_data_with_partial_data_only() : void{
+
+		$c = $this->set_access('device 123');
+		$company_id = $this->set_default_company();
+
+		$product = $this->addNewProduct($company_id);
+		
+		$response = $this->patch('/api/manage-products/'.$product->id, [
+			'product_name'				=>	'updated product name here',
+			'price'						=>	'58.44',
+			'sku'						=>	'updated sku here',
+			'company_id'				=>	$company_id
+		], $c['headers']);
+
+		$response->assertStatus(200);
+
+		$json = $response->json();
+
+		$this->arrayHasKey('validity', $json);
+		$this->assertEquals('updated_success', $json['validity']);
+
+		$updated_product = Product::where('id', '=', $product->id)->first();
+		$this->assertEquals('updated product name here', $updated_product->product_name);
+		$this->assertEquals(58.44, $updated_product->price);
+		$this->assertEquals('updated sku here', $updated_product->sku);
+		
+	}
+
+	public function test_if_it_updates_product_with_valid_data_with_all_data() : void{
+
+		$c = $this->set_access('device 123');
+		$company_id = $this->set_default_company();
+
+		$product = $this->addNewProduct($company_id);
+		
+		$response = $this->patch('/api/manage-products/'.$product->id, [
+			'product_name'				=>	'updated product name here',
+			'price'						=>	'58.44',
+			'sku'						=>	'updated sku here',
+			'description'				=>	'updated description here',
+			'company_id'				=>	$company_id
+		], $c['headers']);
+
+		$response->assertStatus(200);
+
+		$json = $response->json();
+
+		$this->arrayHasKey('validity', $json);
+		$this->assertEquals('updated_success', $json['validity']);
+
+		$updated_product = Product::where('id', '=', $product->id)->first();
+		$this->assertEquals('updated product name here', $updated_product->product_name);
+		$this->assertEquals(58.44, $updated_product->price);
+		$this->assertEquals('updated sku here', $updated_product->sku);
+		$this->assertEquals('updated description here', $updated_product->description);
+		$this->assertNull($updated_product->deleted_at);
+		
+	}
 
 }
