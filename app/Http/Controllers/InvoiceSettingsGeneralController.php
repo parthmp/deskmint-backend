372f42361764010b6bc9a7281fe5f6bcecce58f5
parthmp <parthparmaronline@gmail.com>
2025-09-26 01:11:53 +0530

    save and update invoice general settings

--- app/Http/Controllers/InvoiceSettingsGeneralController.php
+++ app/Http/Controllers/InvoiceSettingsGeneralController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\General;
+use App\Helpers\Sanitize;
 use App\Models\SettingsInvoice;
 use App\Models\SettingsSection;
 use App\Traits\SettingsDefault;
@@ -33,14 +34,19 @@ class InvoiceSettingsGeneralController extends Controller{
 
 		try{
 
-			$settings = SettingsSection::where('type', '=', 'invoice')->first();
+			$company_id = Sanitize::input($request->input('company_id'));
+
+			$settings = SettingsSection::where([['type', '=', 'invoice_general'], ['company_id', '=', $company_id]])->first();
 
 			if($settings){
-				return $settings->settings_json;
+				return 	[
+						'settings' 	=> 	json_decode($settings->settings_json, true),
+						'templates'	=>	$this->fetchTemplates()
+					];
 			}
 
 			return 	[
-						'settings' 	=> $this->getDefaultInvoiceGeneralSettings(),
+						'settings' 	=> 	$this->getDefaultInvoiceGeneralSettings(),
 						'templates'	=>	$this->fetchTemplates()
 					];
 					
@@ -52,10 +58,54 @@ class InvoiceSettingsGeneralController extends Controller{
 
 	public function saveOrUpdate(Request $request){
 
-		// $v = Validator::make($request->all(), [
-		// 	'template'	=>	'required',
+		$v = Validator::make($request->all(), [
+			'template'			=>	'required',
+			'font_size'			=>	'required|numeric',
+			'logo_size'			=>	'required|numeric',
+			'primary_color'		=>	'required|hex_color',
+			'secondary_color'	=>	'required|hex_color'
+		]);
+		
+		if($v->fails()){
+			return response(['message' => 'Please fill in required fields', 'errors' => $request->all(),'validity' => 'invalid_data'], config('global.error_code'));
+		}
 
-		// ]);
+		$company_id = Sanitize::input($request->input('company_id'));
+
+		$template = Sanitize::input($request->input('template'));
+		$font_size = (int)Sanitize::input($request->input('font_size'));
+		$logo_size = (int)Sanitize::input($request->input('logo_size'));
+		$primary_color = Sanitize::input($request->input('primary_color'));
+		$secondary_color = Sanitize::input($request->input('secondary_color'));
+
+		$e_invoice = $request->boolean('e_invoice');
+
+		$json = json_encode([
+				'template'				=>	$template,
+				'font_size'				=>	$font_size,
+				'logo_size'				=>	$logo_size,
+				'primary_color'			=>	$primary_color,
+				'secondary_color'		=>	$secondary_color,
+				'e_invoice_on'			=>	$e_invoice
+			]);
+		
+		$setting = SettingsSection::where([['type', '=', 'invoice_general'], ['company_id', '=', $company_id]])->first();
+
+		if($setting){
+			$obj = $setting;
+		}else{
+			$obj = new SettingsSection();
+			$obj->company_id = $company_id;
+			$obj->type = 'invoice_general';
+		}
+
+		$obj->settings_json = $json;
+
+		if($obj->save()){
+			return response(['message' => 'Settings saved successfully', 'validity' => 'saved_success'], 200);
+		}
+
+
 
 	}
     
