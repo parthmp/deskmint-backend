1189c64f3cbea363fb552c94becfe49be13ea15b
parthmp <parthparmaronline@gmail.com>
2025-09-29 22:13:02 +0530

    validation for client details settings

--- app/Http/Controllers/InvoiceSettingsClientDetailsController.php
+++ app/Http/Controllers/InvoiceSettingsClientDetailsController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Helpers\General;
 use App\Helpers\Sanitize;
+use App\Models\ClientsCustomField;
 use App\Models\SettingsSection;
 use App\Traits\SettingsDefault;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
 
 class InvoiceSettingsClientDetailsController extends Controller{
@@ -36,32 +38,38 @@ class InvoiceSettingsClientDetailsController extends Controller{
 
 	private function validateClientSettingsPost(array $rows) : bool{
 
-		$valid = true;
-
 		$default = $this->getDefaultInvoiceClientDetailsSettings();
 		$default = array_merge($default['dropdown'], $default['rows']);
 
 		foreach($rows as $row){
-			/*
-				{
-					"id": 8,
-					"text": "Email",
-					"value": "email",
-					"mapped": [
-						"email"
-					],
-					"type": "normal"
-				}
-			*/
+
 			if($row['type'] === 'normal'){
+				
+				if(!is_array($row['mapped'])){
+					return false;
+				}
+
+				$client_table_columns = Schema::getColumnListing('clients');
+				foreach($row['mapped'] as $mapped_col){
+					if(!in_array($mapped_col, $client_table_columns)){
+						return false;
+					}
+				}
 
 			}else{
 				
+				/* if custom fields / columns added */
+				$custom_fields_ids = ClientsCustomField::pluck('id')->toArray();
+				if(!in_array($row['clients_custom_fields'], $custom_fields_ids)){
+					return false;
+				}
+				
+
 			}
 
 		}
 
-		return $valid;
+		return true;
 	}
 
 	public function saveOrUpdate(Request $request){
@@ -75,23 +83,40 @@ class InvoiceSettingsClientDetailsController extends Controller{
 		]);
 
 		if($v->fails()){
-			return response(['message' => json_encode($v->errors()),'validity' => 'invalid_data'], config('global.error_code'));
+			return response(['message' => 'invalid request','validity' => 'invalid_data'], config('global.error_code'));
 		}
 
-		/* now validate before moving forward */
+		try{
+
+			$rows = $request->input('rows');
+
+			/* now validate before moving forward */
+			if(!$this->validateClientSettingsPost($rows)){
+				return response(['message' => 'invalid request','validity' => 'bad_request'], config('global.error_code'));
+			}
 
 
-		$company_id = Sanitize::input($request->input('company_id'));
+			$company_id = Sanitize::input($request->input('company_id'));
 
-		$settings = SettingsSection::where([['type', '=', 'invoice_client_details'], ['company_id', '=', $company_id]])->first();
+			$settings = SettingsSection::where([['type', '=', 'invoice_client_details'], ['company_id', '=', $company_id]])->first();
 
-		if($settings){
-			$s = $settings;
-		}else{
-			$s = new SettingsSection();
+			if($settings){
+				$s = $settings;
+			}else{
+				$s = new SettingsSection();
+				$s->company_id = $company_id;
+				$s->type = 'invoice_client_details';
+			}
+
+			$s->settings_json = json_encode($rows);
+
+			if($s->save()){
+				return response(['message' => 'Saved successfully','validity' => 'save_success'], 200);
+			}
+
+		}catch(Exception $e){
+			return General::wentWrong();
 		}
-
-
 
 	}
 
