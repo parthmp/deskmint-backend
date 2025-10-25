29e0fa9dc1cab293289285542697ab9e30e1fc16
parthmp <parthparmaronline@gmail.com>
2025-10-26 01:09:30 +0530

    day end

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -26,13 +26,16 @@ class InvoiceController extends Controller{
 		$company_id = Sanitize::input($request->input('company_id'));
 		$searched = Sanitize::input($request->input('searched'));
 
-		$clients = Client::select('id', 'first_name', 'last_name')->where('company_id', '=', $company_id)->where(function($query) use($searched){
+		$clients = Client::select('id', 'first_name', 'last_name', 'currency_id')->where('company_id', '=', $company_id)->where(function($query) use($searched){
 			$query->where('first_name', 'LIKE', '%'.$searched.'%');
 			$query->orwhere('last_name', 'LIKE', '%'.$searched.'%');
-		})->orderBy('first_name', 'ASC')->limit(50)->get()->map(function($client){
+		})->with('currency')->orderBy('first_name', 'ASC')->limit(50)->get()->map(function($client){
 			return [
 				'text'		=>	$client->first_name.' '.$client->last_name,
-				'value'		=>	$client->id
+				'value'		=>	$client->id,
+				'data'		=>	[
+					'currency'	=>	$client->currency
+				]
 			];
 		})->toArray();
 
@@ -80,12 +83,15 @@ class InvoiceController extends Controller{
 		$company_id = Sanitize::input($request->input('company_id'));
 		$searched = Sanitize::input($request->input('searched'));
 
-		$products = Product::select('id', 'product_name')->where('company_id', '=', $company_id)->where(function($query) use($searched){
+		$products = Product::select('id', 'product_name', 'description', 'price')->where('company_id', '=', $company_id)->where(function($query) use($searched){
 			$query->where('product_name', 'LIKE', '%'.$searched.'%');
 		})->orderBy('product_name', 'ASC')->limit(50)->get()->map(function($product){
 			return [
 				'text'		=>	$product->product_name,
-				'value'		=>	$product->id
+				'value'		=>	$product->id,
+				'data'		=>	[
+					'product' => $product
+				]
 			];
 		})->toArray();
 
