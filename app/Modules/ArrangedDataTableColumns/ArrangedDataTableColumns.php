4ef8b8ef2c1559b773a09eedc974b9108dd0793e
parthmp <parthparmaronline@gmail.com>
2026-02-01 22:47:53 +0530

    ArrangedDataTableColumns orchestration class

--- app/Modules/ArrangedDataTableColumns/ArrangedDataTableColumns.php
+++ app/Modules/ArrangedDataTableColumns/ArrangedDataTableColumns.php
@@ -2,7 +2,40 @@
 
 namespace App\Modules\ArrangedDataTableColumns;
 
+use App\Modules\ArrangedDataTableColumns\Actions\Fetch;
+use App\Modules\ArrangedDataTableColumns\Actions\Save;
+use Illuminate\Http\Request;
 
 class ArrangedDataTableColumns{
+
+	public function __construct(private Fetch $fetch, private Save $save){}
+
+	/**
+	 * fetchArrangedColumnsData function
+	 *
+	 * @param Request $request
+	 * @param string $feature_name
+	 * @param string $original_table
+	 * @param string $custom_fields_model
+	 * @param string $type
+	 * @return array
+	 */
+	public function fetchArrangedColumnsData(Request $request, string $feature_name, string $original_table, string $custom_fields_model, string $type) : array {
+		return $this->fetch->fetchArrangedColumnsData($request, $feature_name, $original_table, $custom_fields_model, $type);
+	}
+	
+	/**
+	 * saveArrangedColumnsData function
+	 *
+	 * @param Request $request
+	 * @param string $custom_fields_model
+	 * @param string $feature_name
+	 * @param string $original_table
+	 * @param string $type
+	 * @return boolean
+	 */
+	public function saveArrangedColumnsData(Request $request, string $custom_fields_model, string $feature_name, string $original_table, string $type) : bool {
+		return $this->save->saveArrangedColumnsData($request, $custom_fields_model, $feature_name, $original_table, $type);
+	}
 
 }
