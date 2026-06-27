21d601adcce468246c7e0311422e5c6e63d7906f
parthmp <parthparmaronline@gmail.com>
2026-06-28 00:27:24 +0530

    logo functionality change - init for snapshot changes for invoices

--- app/Services/CompanySettingsLogo/CompanySettingsLogoService.php
+++ app/Services/CompanySettingsLogo/CompanySettingsLogoService.php
@@ -27,7 +27,7 @@ class CompanySettingsLogoService{
 
 		$image_url = '';
 
-		if(Storage::disk('public')->exists($logo_file)){
+		if(Storage::disk('public')->exists($logo_file) && trim($company->logo) !== ''){
 			$image_url = Storage::disk('public')->url($logo_file);
 		}
 
@@ -50,7 +50,8 @@ class CompanySettingsLogoService{
 		$extension = $file->getClientOriginalExtension();
 		$filename = md5(time().'_'.$file->getClientOriginalName()).'.'.$extension;
 		
-		Storage::disk('public')->deleteDirectory($path);
+		//do not remove logo file as we are using it for invoice snapshot.
+		// Storage::disk('public')->deleteDirectory($path);
 		Storage::disk('public')->makeDirectory($path);
 
 		Storage::disk('public')->putFileAs($path, $file, $filename);
@@ -73,9 +74,10 @@ class CompanySettingsLogoService{
 
 			$path = 'logos/'.$company_id;
 
-			if(Storage::disk('public')->exists($path)){
-				Storage::disk('public')->deleteDirectory($path);
-			}
+			//do not remove logo file as we are using it for invoice snapshot.
+			// if(Storage::disk('public')->exists($path)){
+			// 	Storage::disk('public')->deleteDirectory($path);
+			// }
 
 			$company = $this->company_repository->fetchDefaultById($company_id);
 			return $this->company_repository->updateCompanyLogoByObj('', $company);
